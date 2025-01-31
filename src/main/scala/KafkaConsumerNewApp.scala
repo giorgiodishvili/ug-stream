0d586d4a62db1c7eac58d99e60d255ad82f244d1
giorgiodishvili <odishvili.giorgi@gmail.com>
2025-01-31 11:41:23 +0400

    changed processing

--- src/main/scala/KafkaConsumerNewApp.scala
+++ src/main/scala/KafkaConsumerNewApp.scala
@@ -1,7 +1,8 @@
-import org.apache.spark.{SparkConf, SparkContext}
 import org.apache.spark.sql.{Dataset, Row, SparkSession}
-import org.apache.spark.sql.functions.{col, current_timestamp, from_json, lit, sum}
+import org.apache.spark.sql.functions.{col, current_timestamp, from_json, sum}
 import org.apache.spark.sql.types.{IntegerType, StringType, StructField, StructType}
+import org.apache.spark.SparkConf
+import org.apache.spark.SparkContext
 
 object KafkaConsumerNewApp extends App {
   override def main(args: Array[String]): Unit = {
@@ -16,20 +17,18 @@ object KafkaConsumerNewApp extends App {
 
     import spark.implicits._
 
-    // Define schema for incoming Kafka data
-    val schema = StructType(
-      List(
-        StructField("EmpId", IntegerType, true),
-        StructField("EmpName", StringType, true),
-        StructField("DeptName", StringType, true),
-        StructField("Salary", IntegerType, true)
-      )
-    )
+    // Define schema for Kafka messages
+    val schema = StructType(List(
+      StructField("EmpId", IntegerType, true),
+      StructField("EmpName", StringType, true),
+      StructField("DeptName", StringType, true),
+      StructField("Salary", IntegerType, true)
+    ))
 
     // Read department table from MySQL
     val deptTable = spark.read.format("jdbc")
       .option("url", "jdbc:mysql://localhost:3306/mydb")
-      .option("dbtable", "department") // MySQL table with columns `id` and `name`
+      .option("dbtable", "department")
       .option("user", "root")
       .option("password", "rootpassword")
       .load()
@@ -44,42 +43,44 @@ object KafkaConsumerNewApp extends App {
       .option("subscribe", "kafkaTopic")
       .load()
 
-    // Parse and flatten Kafka JSON stream
+    // Parse Kafka JSON messages
     val FlattenKafkaStream = kafkaStream.selectExpr("CAST(value AS STRING) as json")
       .select(from_json(col("json"), schema).alias("tmp"))
       .select("tmp.*")
 
     // Join streamed data with department table to fetch Department ID
     val joinedStream = FlattenKafkaStream
-      .join(deptTable, Seq("DeptName"), "inner") // Use inner join to drop unmatched departments
+      .join(deptTable, Seq("DeptName"), "inner") // Ensures only valid departments are considered
       .select(col("DeptId"), col("Salary"))
 
-    // Aggregate the data by Department ID
+    // Aggregate data by Department ID
     val aggregation = joinedStream
       .groupBy(col("DeptId"))
       .agg(sum("Salary").alias("TotalSalary"))
 
-    // Save to MySQL
+    // Save to MySQL (MySQL will auto-generate the primary key)
     def saveToMySql = (df: Dataset[Row], batchId: Long) => {
-      val url = """jdbc:mysql://localhost:3306/mydb"""
+      val url = "jdbc:mysql://localhost:3306/mydb"
 
-      // Add batch metadata and save the aggregated data
       df
-        .withColumn("batchId", lit(batchId))
-        .withColumn("updated_at", current_timestamp())
-        .write.format("jdbc")
+        .withColumn("updated_at", current_timestamp()) // Timestamp column
+        .write
+        .format("jdbc")
         .option("url", url)
-        .option("dbtable", "aggregated_data") // Target table to save the aggregated data
+        .option("dbtable", "aggregated_data") // Auto-increment primary key in MySQL
         .option("user", "root")
         .option("password", "rootpassword")
-        .mode("append")
+        .option("driver", "com.mysql.cj.jdbc.Driver")
+        .option("rewriteBatchedStatements", "true")
+        .option("batchsize", "10000")
+        .mode("append") // Ensures each batch appends new records
         .save()
     }
 
     // Write the stream to MySQL
     val kafkaToMySql = aggregation
       .writeStream
-      .outputMode("complete")
+      .outputMode("complete") // Ensures all data is stored correctly
       .foreachBatch(saveToMySql)
       .start()
       .awaitTermination()
